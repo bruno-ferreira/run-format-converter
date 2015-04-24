e3bb7c8d39594a59f15c187c0e89ce8116afa328
Bruno Ferreira <bruno.ferreira@ndrive.com>
2015-04-24 17:59:54 +0100

    xml parse definition

--- run-format-converter.py
+++ run-format-converter.py
@@ -2,7 +2,7 @@
 ''' docstring '''
 
 import argparse
-
+import os
 from xml.etree import ElementTree as ET
 
 # create a class that opens and reads any xml format
@@ -11,24 +11,44 @@ from xml.etree import ElementTree as ET
 # etc
 
 
+class openXMLFile(object):
+    '''docstring'''
+    def open():
+        tree = ET.parse(self)
+        root = tree.getroot()
+
+
 def pwx_reader(a_file):
     '''docstring'''
+    a_file = os.path.abspath(a_file)
     tree = ET.parse(a_file)
     root = tree.getroot()
     print root
+    print root.tag  # main tag
+    print root.attrib  # main attribute
+    for child in root:
+        print child.tag, child.attrib
 
 
 def main():
     '''docstring'''
-    parser = argparse.ArgumentParser(description='Converts run files formats')
-
-    parser.add_argument('--file', help='run file that will be converted')
-    parser.add_argument('--format', help='output format of run file')
-
-    args = parser.parse_args()
-
-    print args
-    pwx_reader(args['file'])
+    # parser = argparse.ArgumentParser(description='Converts run files formats')
+    #
+    # parser.add_argument('-f', '--file', help='run file that will be converted')
+    # parser.add_argument('-t', '--type', help='output type of run file')
+    #
+    # args = parser.parse_args()
+    #
+    # print args
+    # pwx_reader(args['file'])
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Converts run files formats')
+
+    parser.add_argument('-f', '--file', help='run file that will be converted')
+    parser.add_argument('-t', '--type', help='output type of run file')
+
+    args = vars(parser.parse_args())
+
+#    print args
+    pwx_reader(args['file'])
